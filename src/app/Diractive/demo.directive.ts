91422fa97335707db3e033372d610d497476bfde
yasser taha <yasser1119992017@gmail.com>
2024-09-12 19:21:41 +0300

    install new project with bootstrap and create four components

--- src/app/Diractive/demo.directive.ts
+++ src/app/Diractive/demo.directive.ts
@@ -1,14 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, input } from '@angular/core';
 
 @Directive({
   selector: '[appDemo]',
   standalone: true
 })
 export class DemoDirective {
+  @Input('appDemo') myobj={bgcolor:"",fgcolor:"",tAlign:""};
 
-  constructor() {
-    console.log('efuwiiiiiiiiiiiiioo')
-
+  constructor(private myelement:ElementRef) {
+    // console.log('efuwiiiiiiiiiiiiioo')
+    // console.log(myelement);
+    myelement.nativeElement.style.backgroundColor="black";
+    myelement.nativeElement.style.color="red";
+    myelement.nativeElement.style.textAlign="right";
+   }
+   @HostListener('click')oncheng(){
+    this.myelement.nativeElement.style.backgroundColor=this.myobj.bgcolor;
+    this.myelement.nativeElement.style.color=this.myobj.fgcolor;
+    this.myelement.nativeElement.style.textAlign=this.myobj.tAlign;
    }
 
 }
---
+++ src/app/component/users-detials/users-detials.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-detials',
+  standalone: true,
+  imports: [],
+  templateUrl: './users-detials.component.html',
+  styles: ``
+})
+export class UsersDetialsComponent {
+
+}
