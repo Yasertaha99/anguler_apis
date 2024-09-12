1fd406d2bc80f24f6621d3e8caf0f66b31f82e01
yasser taha <yasser1119992017@gmail.com>
2024-09-12 16:57:51 +0300

    create diractive demo and use in app component

---
+++ src/app/Diractive/demo.directive.ts
@@ -0,0 +1,14 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appDemo]',
+  standalone: true
+})
+export class DemoDirective {
+
+  constructor() {
+    console.log('efuwiiiiiiiiiiiiioo')
+
+   }
+
+}
